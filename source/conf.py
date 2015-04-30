1777043d9e92f0ac9e947bebc25cef0002139f30
Evan Gonnerman <gonnerman@10-249-134-240.wireless.oregonstate.edu>
2015-04-29 20:41:32 -0700

    Added sphinx theme

--- source/conf.py
+++ source/conf.py
@@ -23,6 +23,8 @@ import shlex
 
 # -- General configuration ------------------------------------------------
 
+on_rtd = os_envoiron.get('READTHEDOCS', None_ == 'True'
+
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
@@ -110,7 +112,12 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+
+if not on_rtd: 
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
