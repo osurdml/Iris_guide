62f30f558943b4cdd8d5248a3c7a565a0f1a898a
Evan Gonnerman <gonnerman@10-249-134-240.wireless.oregonstate.edu>
2015-04-29 20:47:01 -0700

    Reverted to original documentation

--- source/conf.py
+++ source/conf.py
@@ -23,8 +23,6 @@ import shlex
 
 # -- General configuration ------------------------------------------------
 
-on_rtd = os_environ.get('READTHEDOCS', None) == 'True'
-
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
@@ -113,11 +111,6 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster'
-
-if not on_rtd: 
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
